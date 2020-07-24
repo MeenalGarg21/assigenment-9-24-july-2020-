2bd886c00974cbf4b610befabb15c9980456500d
MeenalGarg <meenal.garg@zensar.com>
2020-07-24 11:24:22 -0700

    assigenment-9

---
+++ MathFunctionInJs.js
@@ -0,0 +1,23 @@
+'use strict'
+//Power returns the value of x to the power of y:
+var x=2
+var y=3
+console.log(Math.pow(x,y))
+console.log(`using the maths power function=${Math.pow(x,y)}`)
+//ceil returns the value of x rounded up to its nearest integer:
+var x=2.2
+//var y=3.9999
+console.log(Math.ceil(x))
+console.log(`using the maths ceil function=${Math.ceil(x)}`)
+//round returns the rounded value
+var x=2.29999999
+//var y=3.9999
+console.log(Math.round(x))
+console.log(`using the maths round function=${Math.round(x)}`)
+// square root
+var x=16
+console.log(Math.sqrt(x))
+console.log(`using the maths sqrt function=${Math.sqrt(x)}`)
+console.log(`using the maths sin function=${Math.sin(x)}`)
+
+
